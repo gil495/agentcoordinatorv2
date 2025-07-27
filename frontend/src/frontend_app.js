4d7e7d6d2c99cd4be6bf8c6aafc9ae38b0add1ef
gil495 <gilbertw5000@gmail.com>
2025-07-26 23:20:44 -0400

    Update 2

---
+++ frontend/src/frontend_app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatInterface from './ChatInterface';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatInterface />
+    </div>
+  );
+}
+
+export default App;
